296d82072f829d518928bae61fe63054240bdc5a
Jason <quietjason@gmail.com>
2019-02-26 11:38:30 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,10 @@
+
+const isDev = (process.env.NODE_ENV === "development")
+
 module.exports = {
-  publicPath: '/editor/'
+  publicPath: isDev ? '/' : '/editor/',
+  productionSourceMap: isDev ? true : false,
+  configureWebpack: {
+    devtool: isDev ? 'source-map' : false
+  }
 }
