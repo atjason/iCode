7dd6a8e987b8aa2eae575ac850b95206c7b361eb
Jason <quietjason@gmail.com>
2019-02-27 18:05:44 +0800

    Use cdn for vue.

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,13 @@ const isDev = (process.env.NODE_ENV === 'development')
 module.exports = {
   publicPath: isDev ? '/' : '/editor/',
   productionSourceMap: isDev,
-  configureWebpack: {
-    devtool: isDev ? 'source-map' : false
+  configureWebpack: config => {
+    config.devtool = isDev ? 'source-map' : false
+    if (!isDev) {
+      config.externals = {
+        'vue': 'Vue',
+        'vue-router': 'VueRouter'
+      }
+    }
   }
 }
